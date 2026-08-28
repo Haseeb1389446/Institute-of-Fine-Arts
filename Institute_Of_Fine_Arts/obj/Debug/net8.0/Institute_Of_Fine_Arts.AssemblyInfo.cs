d137be1076c2cb0c76854f5a3966f4e617197dd3
Abdul Haseeb <haseeb1389446@gmail.com>
2026-08-28 16:05:37 +0500

    UI Modification

--- Institute_Of_Fine_Arts/obj/Debug/net8.0/Institute_Of_Fine_Arts.AssemblyInfo.cs
+++ Institute_Of_Fine_Arts/obj/Debug/net8.0/Institute_Of_Fine_Arts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Institute_Of_Fine_Arts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25bf5907ae6d5f5b0687da82640626d1d89bf755")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc094b94fb916cb4245284126e219a8b77d9fff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Institute_Of_Fine_Arts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Institute_Of_Fine_Arts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
